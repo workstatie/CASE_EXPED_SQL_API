d8cc40cdae2e4d9cef84ac7d18b7241845e331dc
Mihai Andrei <mihai.andrei@uipath.com>
2020-05-17 23:58:33 +0300

    changed update

--- src/routes/Request.js
+++ src/routes/Request.js
@@ -42,7 +42,7 @@ router.get('/GetAllRequests', function (req, res, next) {
             console.log(err);
         }
         var request = new sql.Request();
-        request.query('select * from ' + tableName , function (err, recordset) {
+        request.query('select * from ' + tableName, function (err, recordset) {
             if (err) {
                 console.log(err)
             }
@@ -73,88 +73,64 @@ router.get('/GetRequestDetails', function (req, res, next) {
 router.post('/NewRequest', function (req, res) {
     const request = req.body;
 
-        sql.connect(dbconfig, function (err) {
+    sql.connect(dbconfig, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        let sqlQueryPost = "INSERT INTO " + tableName + " ("
+
+        for (var i = 0; i < Object.keys(req.body).length; i++) {
+            sqlQueryPost = sqlQueryPost + Object.keys(req.body)[i] + ",";
+        }
+
+        sqlQueryPost = sqlQueryPost + "  datetime_created, datetime_modified)  OUTPUT SCOPE_IDENTITY()  VALUES ( '"
+
+        for (var i = 0; i < Object.keys(req.body).length; i++) {
+            sqlQueryPost = sqlQueryPost + Object.values(req.body)[i] + "','"
+        }
+        sqlQueryPost = sqlQueryPost.slice(0, sqlQueryPost.length - 2) + ", GETDATE(), GETDATE()) SELECT SCOPE_IDENTITY() as id";
+
+        var sqlRequest = new sql.Request();
+
+        sqlRequest.query(sqlQueryPost, function (err, recordset) {
             if (err) {
-                console.log(err);
+                res.status(400)
+                res.send(err);
+                console.log(err)
+            } else {
+                res.status(200)
+                res.send(recordset)
             }
-            let sqlQueryPost = "INSERT INTO " +tableName +" ("
-
-            for (var i = 0; i < Object.keys(req.body).length; i++) {
-                sqlQueryPost = sqlQueryPost + Object.keys(req.body)[i] + ",";
-            }
-
-            sqlQueryPost= sqlQueryPost + "  datetime_created, datetime_modified)  OUTPUT SCOPE_IDENTITY()  VALUES ( '"
-
-            for (var i = 0; i < Object.keys(req.body).length; i++) {
-                sqlQueryPost = sqlQueryPost+ Object.values(req.body)[i] + "','"
-            }
-            sqlQueryPost = sqlQueryPost.slice(0, sqlQueryPost.length - 2) + ", GETDATE(), GETDATE()) SELECT SCOPE_IDENTITY() as id";
-            
-            var sqlRequest = new sql.Request();
-
-            sqlRequest.query(sqlQueryPost, function (err, recordset) {
-                if (err) {
-                    res.status(400)
-                    res.send(err);
-                    console.log(err)
-                } else {
-                    res.status(200)
-                    res.send(recordset)
-                }
-            });
         });
+    });
 });
 
 //Update Request by ID
 router.put('/UpdateRequest', function (req, res) {
-    const request = req.body;
-    if (Object.keys(request).length != 18) {
-        res.status(400)
-        res.send('Request Failed - expected 18 fields, received ' + Object.keys(request).length);
-    }
-    else {
-        sql.connect(dbconfig, function (err) {
-            if (err) {
-                console.log(err);
-            }
-            var sqlRequest = new sql.Request();
-            const sqlQuery = "UPDATE " + tableName + " SET \
-            customer_id = '" + request.customer_id + "' ,  \
-            from_address_city = '" + request.from_address_city + "' , \
-            from_address_postcode = '" + request.from_address_postcode + "' , \
-            from_address_country = '" + request.from_address_country + "' , \
-            to_address_city = '" + request.to_address_city + "' ,\
-            to_address_postcode = '" + request.to_address_postcode + "' ,\
-            to_address_country = '" + request.to_address_postcode + "' ,\
-            load_datetime = '" + request.load_datetime + "' ,\
-            unload_datetime = '" + request.unload_datetime + "' , \
-            solution_time = '" + request.solution_time + "' ,\
-            customer_agreed_solution_time = '" + request.solution_time + "' ,\
-            validation_datetime = '" + request.validation_datetime + "' , \
-            goods_weight = '" + request.goods_weight + "', \
-            goods_europallets = '" + request.goods_europallets + "', \
-            truck_type_id = '" + request.truck_type_id + "' , \
-            adr_type_id = '" + request.adr_type_id + "' , \
-            special_requirments = '" + request.special_requirments + "' , \
-            email_html = '" + request.email_html + "' , \
-            assigned_user_id = '" + request.assigned_user_id + "' , \
-            request_status_type_id = '" + request.request_status_type_id + "' , \
-            datetime_modified = GETDATE() \
-            WHERE id = " + req.query.id;
 
-            sqlRequest.query(sqlQuery, function (err, recordset) {
-                if (err) {
-                    res.status(400)
-                    res.send(err);
-                    console.log(err)
-                } else {
-                    res.status(200)
-                    res.send('updated')
-                    // /    res.send('inserting data in SQL table ' + tableName);
-                }
-            });
+    sql.connect(dbconfig, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        let sqlQueryPut = "UPDATE [Request] SET "
+
+        for (var i = 0; i < Object.keys(req.body).length; i++) {
+            sqlQueryPut = sqlQueryPut + Object.keys(req.body)[i] + " = '" + Object.values(req.body)[i] + "',"
+        }
+        sqlQueryPut = sqlQueryPut.slice(0, sqlQueryPut.length - 1) + " WHERE ID = " + req.query.id;
+        var sqlRequest = new sql.Request();
+
+        sqlRequest.query(sqlQueryPut, function (err, recordset) {
+            if (err) {
+                res.status(400)
+                res.send(err);
+                console.log(err)
+            } else {
+                res.status(200)
+                res.send('updated')
+            }
         });
-    }
+    });
 });
 
 
