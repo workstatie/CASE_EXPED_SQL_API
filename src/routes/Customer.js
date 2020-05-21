1681df003a3f6a082492abe3456bae7082203411
Mihai Andrei <mihai.andrei@uipath.com>
2020-05-21 18:07:11 +0300

    added customer contact

--- src/routes/Customer.js
+++ src/routes/Customer.js
@@ -65,6 +65,24 @@ router.get('/GetCustomerByName', function (req, res, next) {
     });
 });
 
+router.get('/GetCustomerContactByCustomerID', function (req, res, next) {
+    sql.connect(dbconfig, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        
+        var request = new sql.Request();
+
+        const sqlQuery="select * from [Customer_Contact] WHERE customer_id = '" + req.query.customer_id +"'"
+        request.query(sqlQuery, function (err, recordset) {
+            if (err) {
+                console.log(err)
+            }
+            res.send(recordset);
+        });
+    });
+});
+
 
 //Get Customer by Company Name
 router.get('/GetCustomerByID', function (req, res, next) {
@@ -119,6 +137,43 @@ router.post('/CreateCustomer', function (req, res) {
             });
         });
     
+});
+
+
+//Create new Customer 
+router.post('/CreateCustomerContact', function (req, res) {
+   
+    sql.connect(dbconfig, function (err) {
+        if (err) {
+            console.log(err);
+        }
+
+        let sqlQueryPost = "INSERT INTO [Customer_Contact] ("
+
+        for (var i = 0; i < Object.keys(req.body).length; i++) {
+            sqlQueryPost = sqlQueryPost + Object.keys(req.body)[i] + ",";
+        }
+
+        sqlQueryPost= sqlQueryPost + "  datetime_created, datetime_modified)  OUTPUT SCOPE_IDENTITY()  VALUES ( '"
+
+        for (var i = 0; i < Object.keys(req.body).length; i++) {
+            sqlQueryPost = sqlQueryPost+ Object.values(req.body)[i] + "','"
+        }
+        sqlQueryPost = sqlQueryPost.slice(0, sqlQueryPost.length - 2) + ", GETDATE(), GETDATE()) SELECT SCOPE_IDENTITY() as id";
+        
+        var sqlRequest = new sql.Request();
+        sqlRequest.query(sqlQueryPost, function (err, recordset) {
+            if (err) {
+                res.status(400)
+                res.send(err);
+                console.log(err)
+            } else {
+                res.status(200)
+                res.send(recordset);
+            }
+        });
+    });
+
 });
 
 
